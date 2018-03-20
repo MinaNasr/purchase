9cffee56c940f5c118152626470a2ad397323e15
tarek <tarekessam1311@gmail.com>
2018-03-20 13:46:05 +0200

    update database

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 
 var app = express();
+var fs = require('fs');
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/souq");
+
+fs.readdirSync(path.join(__dirname,"models")).forEach(function (fileName) {
+    
+    require("./models/" + fileName);
+    
+});
 
 
 // uncomment after placing your favicon in /public
